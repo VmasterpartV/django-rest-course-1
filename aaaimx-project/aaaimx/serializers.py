3a2a4c55b8200b30c6ecc2aaab20a2ad08351129
Ivan Mota <ivan_mota09@hotmail.com>
2021-02-05 19:43:27 -0600

    serializers and viewsets

---
+++ aaaimx-project/aaaimx/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth.models import Group
+from .models import User, Division
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        exclude = []
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = []
